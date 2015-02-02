cc64059938ef1653950e541ded9099682bf21545
Steven Kutsch <skutsch40@engr-134-25.eastern.ewu.edu>
2015-02-02 14:33:35 -0800

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ var date= new Date();
 var current_hour= date.getHours();
 
 console.log(current_hour);
+
+//If Night time
+//Remove 'day' class
+//Add 'night' class
+
+if (current_hour >= 17){
+	$('body').removeClass('day');
+	$('body').addClass('night');
+};
